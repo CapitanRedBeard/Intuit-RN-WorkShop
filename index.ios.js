9fcc7431f59689151edb7d0b9120e6723f49258e
Lichman, Yuriy <Yuriy_Lichman@intuit.com>
2016-06-24 14:51:01 -0700

    Step 2: Added Title, TextInput, Data Binding

--- index.ios.js
+++ index.ios.js
@@ -9,25 +9,59 @@ import {
     AppRegistry,
     StyleSheet,
     Text,
+    TextInput,
     View
 } from 'react-native';
 
 class StockTickerApp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tickerText: ''
+        }
+    }
+
     render() {
         return (
-            <Text style={styles.text}>
-                Stock Ticker
-            </Text>
+            <View style={styles.container}>
+                <Text style={styles.textFields}>
+                    Search for stocks
+                </Text>
+                <Text style={styles.textFields}>
+                    Search by ticker
+                </Text>
+                <TextInput style={styles.searchInput}
+                           placeholder={"INTU, APPL"}
+                           onChangeText={(text) => this.setState({tickerText: text})}
+                           value={this.state.tickerText}
+                />
+                <Text style={styles.textFields}>
+                    {this.state.tickerText}
+                </Text>
+            </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    text: {
-        color: 'black',
-        backgroundColor: 'white',
-        fontSize: 30,
-        margin: 80
+    textFields: {
+        marginBottom: 20,
+        fontSize: 18,
+        textAlign: 'center'
+    },
+    container: {
+        padding: 30,
+        marginTop: 65,
+        alignItems: 'center'
+    },
+    searchInput: {
+        height: 36,
+        padding: 4,
+        marginRight: 5,
+        flex: 1,
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: 'black'
     }
 });
 
